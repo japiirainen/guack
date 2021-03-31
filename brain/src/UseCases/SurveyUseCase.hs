ef765eed6a5d18ec925abe195cad6c56af38dee7
Joona Piirainen <joona.piirainen@gmail.com>
2021-03-31 20:23:00 +0300

    use cases

--- brain/src/UseCases/SurveyUseCase.hs
+++ brain/src/UseCases/SurveyUseCase.hs
@@ -1,17 +1,38 @@
 module UseCases.SurveyUseCase
 (
-  Dom.Survey (..)
-, Dom.SurveyId (..)
+  Survey (..)
+, SurveyId (..)
+, SurveyQuestion (..)
+, SurveyQuestionId (..)
 )
 where
 
-import qualified Data.Map.Strict     as M
-import           Data.Maybe          (fromMaybe)
-import qualified Domain.SurveyDomain as Dom (Survey (..), SurveyId (..))
-
+import qualified Data.Map.Strict             as M
+import           Data.Maybe                  (fromMaybe)
+import           Domain.SurveyDomain         (Survey (..), SurveyId (..))
+import           Domain.SurveyQuestionDomain (SurveyQuestion (..),
+                                              SurveyQuestionId (..))
+import           Numeric.Natural
 import           Polysemy
 import           Polysemy.Error
-import           Polysemy.Input      ()
-import           Polysemy.KVStore    (KVStore (..))
-import           Polysemy.Trace      (Trace, trace)
+import           Polysemy.Input              ()
+import           Polysemy.KVStore            (KVStore (..))
+import           Polysemy.Trace              (Trace, trace)
 
+
+type Persistence = KVStore SurveyId [SurveyQuestion]
+
+newtype SurveyError = SurveyNotPossible String
+
+
+maxQuestions :: Natural
+maxQuestions = 20
+
+trySurvey :: (Member Persistence r, Member (Error SurveyError) r, Member Trace r) => SurveyId -> [SurveyQuestion] -> Sem r ()
+trySurvey = undefined
+
+fetchQuestions :: (Member Persistence r, Member Trace r) => SurveyId -> Sem r [SurveyQuestion]
+fetchQuestions = undefined
+
+deleteQuestion :: (Member (KVStore SurveyId [SurveyQuestion]) r, Member Trace r) => SurveyQuestion -> Sem r ()
+deleteQuestion = undefined
