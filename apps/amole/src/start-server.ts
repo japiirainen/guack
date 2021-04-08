8a8680ca6c69a3b00e2654f14550492c100c7869
Joona Piirainen <joona.piirainen@gmail.com>
2021-04-08 08:52:44 +0300

    use node runtime

--- apps/amole/src/start-server.ts
+++ apps/amole/src/start-server.ts
@@ -1,15 +1,16 @@
 import * as T from '@effect-ts/core/Effect'
 import { pipe } from '@effect-ts/core/Function'
 import * as Ex from '@effect-ts/express'
+import * as N from '@effect-ts/node/Runtime'
 import { urlencoded, json } from 'body-parser'
 import cors from 'cors'
 
 import { config } from './infrastrucure/config'
-import { error, info } from './infrastrucure/log'
-//import { routes as surveyRoutes } from './Surveys/routes'
+import { info } from './infrastrucure/log'
+import { routes as surveyRoutes } from './Surveys/routes'
 
 const {
-   application: { PORT, HOST },
+   application: { HOST, NAME, PORT },
 } = config
 
 const program = pipe(
@@ -17,9 +18,10 @@ const program = pipe(
       Ex.use(Ex.classic(cors())),
       Ex.use(Ex.classic(urlencoded({ extended: false }))),
       Ex.use(Ex.classic(json()))
-   )
+   ),
+   T.zipRight(surveyRoutes),
+   T.tap(() => T.effectTotal(() => info(`${NAME} running on ${HOST}:${PORT}`))),
+   T.tap(() => T.never)
 )
 
-pipe(program, T.provideSomeLayer(Ex.LiveExpress(HOST, +PORT)), T.runPromiseExit)
-   .then(info(`listening on: ${PORT}`))
-   .catch(e => error(e.message)())
+pipe(program, T.provideSomeLayer(Ex.LiveExpress(HOST, +PORT)), N.runMain)
